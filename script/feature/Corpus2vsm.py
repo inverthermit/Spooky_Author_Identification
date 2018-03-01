9e369be4b7628ae007a25a3cb7795f7f7e784cf8
Tim <l1111j@live.com>
2018-03-02 10:41:02 +1100

    Update: BOW method done. bad performance. Accuracy less than 50%. Gotta try CNN

--- script/feature/Corpus2vsm.py
+++ script/feature/Corpus2vsm.py
@@ -14,25 +14,35 @@ import pandas as pd
 
 sys.path.append('../')
 rootDir = '../../'
-csv_data = pd.read_csv(rootDir+Util.getConfig('train_csv'))
-print(csv_data)
-corpus = csv_data[['text','author']].as_matrix()
+csv_data = pd.read_csv(rootDir+Util.getConfig('train_csv'))#converted to utf-8
+# print(csv_data)
+train_corpus = csv_data[['text','author']].as_matrix()[:,0]
+train_y = csv_data[['text','author']].as_matrix()[:,1]
+# corpus = [x.encode('utf-8') for x in corpus]
+print(train_corpus)
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer()
 
-from sklearn.feature_extraction import DictVectorizer
-vectorizer = DictVectorizer()
-dataset = vectorizer.fit_transform(corpus[:,0])
-print(dataset.toarray())
+X = vectorizer.fit_transform(train_corpus)
+features = vectorizer.get_feature_names()
+print(len(features))
+# print(X.toarray())
 
+
+# dataset = vectorizer.fit_transform(corpus[:,0])
+# print(dataset.toarray())
+#
 from sklearn.ensemble import RandomForestClassifier
-cls = RandomForestClassifier()
-cls.fit(dataset,corpus[:,1])
-cls.score()
+cls = RandomForestClassifier(n_estimators=10)
+# cls.fit(X,train_y)
+# print(cls.score(X,train_y))
 
-# from sklearn.model_selection import cross_val_predict
-# from sklearn.metrics import confusion_matrix
-# y_pred = cross_val_predict(clf,X,y,cv=10)
-# conf_mat = confusion_matrix(y,y_pred)
-# print(conf_mat)
+from sklearn.model_selection import cross_val_predict
+from sklearn.metrics import confusion_matrix
+y_pred = cross_val_predict(cls,X,train_y,cv=10)
+conf_mat = confusion_matrix(train_y,y_pred)
+print(conf_mat)
+
 #
 # from sklearn.metrics import classification_report
 # target_names = ['0', '1']
