c7ec82a2039b5a2b6204b572243f432b2a4bf694
Tim <l1111j@live.com>
2018-03-13 19:31:47 +1100

    Update: starting new features

---
+++ script/RNN/RNN_Keras.py
@@ -0,0 +1,9 @@
+"""
+This file creates the RNN using Keras.
+It is an implementation making use of the hidden relationship between neurons.
+The major task of this is to figure out the time-series relationship between T data points,
+where T might not be a very large number because of the size of the dimension and the structure of the network
+may influence the performance of the network.
+
+Dataset used: time-series.
+"""
